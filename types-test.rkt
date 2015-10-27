d0e035d014f879cc7d0bb865a18c52249c17be70
Oskar Wickström <oskar.wickstrom@live.com>
2015-10-27 07:12:44 +0100

    Use typed expression trees

--- types-test.rkt
+++ types-test.rkt
@@ -13,37 +13,77 @@
     "identity"
     (check-equal?
      ;; (identity true) : bool
-     (get-type (e/app identity (e/var "true")))
-     predef/bool))
+     (get-typed-expr (e/app identity (e/var "true")))
+     (typed-expr
+      predef/bool
+      (e/app
+       (typed-expr
+        (t/fn predef/bool predef/bool)
+        (e/lam
+         (typed-expr predef/bool (e/var "x"))
+         (typed-expr predef/bool (e/var "x"))))
+       (typed-expr predef/bool (e/var "true"))))))
+
 
    (test-case
     "list/singleton"
     (check-equal?
      ;; (list/singleton 1) : (list number)
-     (get-type (e/app (e/var "list/singleton") (e/var "1")))
-     (predef/list predef/number)))
+     (get-typed-expr (e/app (e/var "list/singleton") (e/var "1")))
+     (typed-expr
+      (predef/list predef/number)
+      (e/app
+       (typed-expr
+        (t/fn predef/number (predef/list predef/number))
+        (e/var "list/singleton"))
+       (typed-expr
+        predef/number
+        (e/var "1"))))))
+
 
    (test-case
     "if"
     (check-equal?
-     ;; (if true 1 0) : bool
-     (get-type (e/app
-                (e/app
-                 (e/app (e/var "if") (e/var "true"))
-                 (e/var "1"))
-                (e/var "0")))
+     ;; (if true 1 0) : bool            ;
+     (get-typed-expr (e/app
+                      (e/app
+                       (e/app (e/var "if") (e/var "true"))
+                       (e/var "1"))
+                      (e/var "0")))
+     (typed-expr
+      predef/number
+      (e/app
+       (typed-expr
+        (t/fn predef/number predef/number)
+        (e/app
+         (typed-expr
+          (t/fn predef/number (t/fn predef/number predef/number))
+          (e/app
+           (typed-expr
+            (t/fn predef/bool (t/fn predef/number (t/fn predef/number predef/number)))
+            (e/var "if"))
+           (typed-expr
+            predef/bool
+            (e/var "true"))))
+         (typed-expr
+          predef/number
+          (e/var "1"))))
+       (typed-expr
+        predef/number
+        (e/var "0"))))
      predef/number))
+
 
    (test-case
     "if branches have same type"
     (check-exn
      exn:fail?
-     ;; (if true 1 true)
+     ;; (if true 1 true)                ;
      (lambda ()
-       (get-type (e/app
-                  (e/app
-                   (e/app (e/var "if") (e/var "true"))
-                   (e/var "1"))
-                  (e/var "true"))))))))
+       (get-typed-expr (e/app
+                        (e/app
+                         (e/app (e/var "if") (e/var "true"))
+                         (e/var "1"))
+                        (e/var "true"))))))))
 
 (run-tests types-test)
--- types.rkt
+++ types.rkt
@@ -2,9 +2,12 @@
 
 (require "minikanren/mk.rkt")
 
-(struct e/var (name) #:transparent)
-(struct e/app (fn param) #:transparent)
-(struct e/lam (arg body) #:transparent)
+(define (e/var name)
+  (list ':var name))
+(define (e/app fn param)
+  (list ':app fn param))
+(define (e/lam arg body)
+  (list ':lam arg body))
 
 (define (t/fn domain range)
   (list ':fn domain range))
@@ -12,6 +15,9 @@
   (list ':constructor name args))
 (define (t/failure msg)
   (list ':failure msg))
+
+(define (typed-expr type expr)
+  (list type expr))
 
 (define/match (t/failure? t)
   [((list ':failure _)) #t]
@@ -25,45 +31,89 @@
   [((list ':constructor name '())) name]
   [((list ':constructor name args)) (format "(~a ~a)" name (map show-t args))]
   [((list ':fn domain range)) (format "(-> ~a ~a)" (show-t domain) (show-t range))]
-  [((list ':failure msg)) (format "Failure: ~a" msg)])
+  [((list ':failure msg)) (format "Failure: ~a" msg)]
+  [((? symbol? s)) (~a s)])
+
+(define/match (show-typed-expr type)
+  [((list type (list ':var name))) (format "(: ~a ~a)"
+                                           (show-t type)
+                                           name)]
+  [((list type (list ':app fn param))) (format "(: ~a (~a ~a))"
+                                               (show-t type)
+                                               (show-typed-expr fn)
+                                               (show-typed-expr param))]
+  [((list type (list ':lam arg body))) (format "(: ~a (lambda (~a) ~a))"
+                                               (show-t type)
+                                               (show-typed-expr arg)
+                                               (show-typed-expr body))]
+  [((? symbol? s)) (~a s)])
 
 (define succeed (== #t #t))
 
 (define/match (show-e expr)
-  [((e/var name)) name]
-  [((e/app fn param)) (format "(~a ~a)" (show-e fn) (show-e param))]
-  [((e/lam arg body)) (format "(lambda (~a) ~a)" (show-e arg) (show-e body))])
+  [((list ':var name)) name]
+  [((list ':app fn param)) (format "(~a ~a)" (show-e fn) (show-e param))]
+  [((list ':lam arg body)) (format "(lambda (~a) ~a)" (show-e arg) (show-e body))])
 
 ; predefined types
 (define predef/bool (t/constructor "bool" '()))
 (define predef/number (t/constructor "number" '()))
 (define (predef/list t) (t/constructor "list" (list t)))
 
-; unifies t with the type of the expression
-(define (unify-type t expr scope)
+; unifies t with a typed expression
+(define (type-expr t expr scope)
   (match expr
 
     ;; var
-    [(e/var name) (if (hash-has-key? scope name)
-                      (== t (hash-ref scope name))
+    [(list ':var name) (if (hash-has-key? scope name)
+                      (== t (typed-expr
+                             (hash-ref scope name)
+                             (e/var name)))
                       (let ([n (string->number name)])
                         (if n
-                            (== t predef/number)
+                            (== t (typed-expr predef/number (e/var name)))
                             (== t (t/failure (format "Undefined: ~a" name))))))]
 
     ;; application
-    [(e/app fn param)
-     (fresh (fn-type param-type)
-            (== fn-type (t/fn param-type t))
-            (unify-type fn-type fn scope)
-            (unify-type param-type param scope))]
+    [(list ':app fn param)
+     (fresh (expr-type fn-te fn-type fn-expr-te param-te param-type param-expr-te)
+            ;; the application typed expression itself
+            (== t (typed-expr
+                   expr-type
+                   (e/app fn-te param-te)))
+
+            ;; the type of the application should be the range of the
+            ;; function
+            (== fn-te (typed-expr
+                       (t/fn param-type expr-type)
+                       fn-expr-te))
+            ;; the parameter type should match the domain of the
+            ;; applied function
+            (== param-te (typed-expr
+                          param-type
+                          param-expr-te))
+
+            ;; recursively unify fn and param typed expressions
+            (type-expr fn-te fn scope)
+            (type-expr param-te param scope))]
 
     ;; lambda abstraction
-    [(e/lam (e/var arg-name) body)
-     (fresh (arg-type body-type)
-            (== t (t/fn arg-type body-type))
-            (unify-type
-             body-type
+    [(list ':lam (list ':var arg-name) body)
+     (fresh (arg-te arg-type body-te body-type body-expr-te)
+            (== t (typed-expr
+                   (t/fn arg-type body-type)
+                   (e/lam arg-te body-te)))
+
+
+            (== arg-te (typed-expr
+                        arg-type
+                        (e/var arg-name)))
+            (== body-te (typed-expr
+                         body-type
+                         body-expr-te))
+
+            (type-expr
+             body-te
              body
              (hash-set scope arg-name arg-type)))]
     [e
@@ -78,7 +128,7 @@
   (fresh (a)
          (== t (t/fn predef/bool (t/fn a (t/fn a a))))))
 
-(define (get-type expr)
+(define (get-typed-expr expr)
   (let ([t (run 1 (q)
                 (fresh (singleton-t if-t)
                        (predef/unify-singleton singleton-t)
@@ -88,14 +138,11 @@
                                     "false" predef/bool
                                     "list/singleton" singleton-t
                                     "if" if-t)])
-                   (unify-type q expr predefined-symbols))))])
+                   (type-expr q expr predefined-symbols))))])
     (cond
      [(empty? t) (error "Type check failed:" t)]
      [(t/failure? (first t)) (error (t/failure-message (first t)))]
      [else (first t)])))
-
-(define (show-type expr)
-  (show-t (get-type expr)))
 
 (provide
  e/var
@@ -104,8 +151,9 @@
  t/fn
  t/constructor
  t/failure
- get-type
- show-type
+ typed-expr
+ get-typed-expr
+ show-typed-expr
  predef/bool
  predef/number
  predef/list)
