453b27b88ea767223d7cb5aeed817f4868ecc0fa
Oskar Wickström <oskar.wickstrom@live.com>
2015-11-07 14:46:53 +0100

    Add colors to repl

--- repl.rkt
+++ repl.rkt
@@ -7,12 +7,30 @@
 (define (on-exit-exception e)
   (raise-user-error "\nBye!"))
 
+(define (text-bold)
+  (display "\033[1m"))
+
+(define (text-blue)
+  (display "\033[34m"))
+
+(define (text-red)
+  (display "\033[31m"))
+
+(define (text-reset)
+  (display "\033[m"))
+
 (define (repl)
   (displayln "Welcome to Kashmir! Type CTRL-C to exit.")
   (define (loop)
     (with-handlers ([exn:break? on-exit-exception]
-		    [exn:fail? (lambda (e) (displayln e))])
+		    [exn:fail? (lambda (e)
+				 (text-red)
+				 (displayln (exn-message e))
+				 (text-reset))])
+      (text-bold)
+      (text-blue)
       (display "kashmir> ")
+      (text-reset)
       (let* ([input (read)])
 	(if (eof-object? input)
 	    (newline)
