5172613cbaec6971368a63e0989ea113d53d4f02
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2016-03-19 13:00:08 +0000

    Fix duplicate type signature error (#69).
    * Support the reporting of multiple top-level errors
    * Make error message more descriptive when type signature definition follows value definition

--- src/Oden/Output/Explode.hs
+++ src/Oden/Output/Explode.hs
@@ -11,17 +11,27 @@ instance OdenOutput ExplodeError where
   outputType _ = Error
   name TypeSignatureWithoutDefinition{} =
     "Explode.TypeSignatureWithoutDefinition"
+  name TypeSignatureRedefinition{} =
+    "Explode.TypeSignatureRedefinition"
   name InvalidMemberAccessExpression{} =
     "Explode.InvalidMemberAccessExpression"
 
   header TypeSignatureWithoutDefinition{} _ =
     text "Type signature has no corresponding definition"
+  header TypeSignatureRedefinition{} _ =
+    text "Redundant type signature definition"
   header (InvalidMemberAccessExpression _ _ nonName) s =
     text "Invalid member access expression" <+> code s (pp nonName)
 
   details (TypeSignatureWithoutDefinition _ n ts@(TypeSignature _ _ TSFn{})) s =
     text "Define the function" <+> code s (pp n)
     <+> text "with type" <+> code s (pp ts)
+  details (TypeSignatureRedefinition _ n ts) s
+    = case ts of
+        Just ts' -> text "Duplicate type signature for" <+> code s (pp n)
+                    <+> text ". Already defined as" <+> code s (pp ts')
+        Nothing -> text "Type signature must precede the definition for"
+                   <+> code s (pp n)
   details (InvalidMemberAccessExpression _ expr' _) s =
     text "In the expression" <+> code s (pp expr')
 
@@ -30,4 +40,5 @@ instance OdenOutput ExplodeError where
     <+> text "with type" <+> code s (pp sc)
 
   sourceInfo (TypeSignatureWithoutDefinition si _ _) = Just si
-  sourceInfo (InvalidMemberAccessExpression si _ _) = Just si
+  sourceInfo (TypeSignatureRedefinition si _ _)      = Just si
+  sourceInfo (InvalidMemberAccessExpression si _ _)  = Just si
